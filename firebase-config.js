907a736a5096223f9c5e2e33dfc62117895187b7
VerooMD <carlivianevada@gmail.com>
2026-07-19 08:00:44 +0700

    Add Firebase configuration to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.__FIREBASE_CONFIG__ = {
+  apiKey: "AIzaSyCZdNFFu7RiFO1UJubVF5UJ1fvgpBki3i8",
+  authDomain: "xeandigitalchat.firebaseapp.com",
+  projectId: "xeandigitalchat",
+  storageBucket: "xeandigitalchat.firebasestorage.app",
+  messagingSenderId: "118463572763",
+  appId: "1:118463572763:web:5df9d2a725987d0de601be"
+};
